f61060d0e6668b86ec9c85eec49f9ed6890d8233
Hafizur83 <freelancerhafizur@gmail.com>
2022-03-09 00:06:35 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+//document.getElementById('priceCheck').onclick= function checkmark(){
+//    const price = document.getElementById('price');
+//    console.log(price.value)
+//}
+//
+//$(document).ready(function () {
+//                $('.serviceCheck').on('click', function () {
+//                    if ($(this).is(':checked')) {
+//                        $(this).closest(".serviceWrap").find('.price').prop('readonly', false);
+//                    } else {
+//                        $(this).closest(".serviceWrap").find('.price').prop('readonly', true);
+//                    }
+//                });
+//            });
+
+//$(document).ready(function () {
+//                $('.priceCheck').on('click', function () {
+//                    if($(this).is(':checked')){
+//                        alert('gggg')
+//                    }else{
+//                        alert('nai')
+//                    }
+//                    if ($(this).is(':checked')) {
+//                        $(this).closest(".serviceWrap").find('.price').prop('readonly', false);
+//                    } else {
+//                        $(this).closest(".serviceWrap").find('.price').prop('readonly', true);
+//                    }
+                });
+//            });
